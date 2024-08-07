28678e0dd9b4caf1219c2a53ca3b55377961ca54
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-07 19:57:22 +0500

    Allowing Orders api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors' // Import the cors package
 import { Port } from './Config.js'
 import Router from './DB/ProductCrud.js'
+import { OrderRouter } from './DB/GettingOrder.js'
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/Products', Router)
+app.use('/api/Orders', OrderRouter)
 
 app.listen(Port, () => {
   console.log(`Running on port ${Port}`)
