8a107601fc888290997811a337807cabb8c666d9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-07 19:26:07 +0500

    Added keys in env folder

--- Config.js
+++ Config.js
@@ -1,2 +1,6 @@
 import 'dotenv/config'
-export const Port = process.env.Port
+const Port = process.env.Port
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SECRET
+
+export { Port, supabaseKey, supabaseUrl }
