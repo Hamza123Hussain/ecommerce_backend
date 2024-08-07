ab2a15fdfedfdb7754a1001c67fccc6dfdd37535
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-07 19:34:01 +0500

    allowing router path and parsing json allowed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import { Port } from './Config.js'
+import Router from './DB/ProductCrud.js'
 const app = express()
+
+app.use(express.json())
+
+app.use('/api/Products', Router)
 
 app.listen(Port, () => {
   console.log(`ruuning on ${Port} port`)
