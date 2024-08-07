99d262b9594a40ba9f6c8756feeec2c069449cf4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-07 19:39:08 +0500

    allowing backend for all origins

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express'
+import cors from 'cors' // Import the cors package
 import { Port } from './Config.js'
 import Router from './DB/ProductCrud.js'
+
 const app = express()
+
+// Apply CORS middleware with default options (allow all origins)
+app.use(cors())
 
 app.use(express.json())
 
 app.use('/api/Products', Router)
 
 app.listen(Port, () => {
-  console.log(`ruuning on ${Port} port`)
+  console.log(`Running on port ${Port}`)
 })
